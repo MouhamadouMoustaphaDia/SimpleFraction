3a159de045ecff5261c139e4357b017460611e0c
MouhamadouMoustapha <Mouhamadou Moustapha Dia>
2019-09-18 14:47:52 +0200

    first commit

---
+++ src/Fraction.java
@@ -0,0 +1,20 @@
+
+public class Fraction {
+	int denominateur;
+	int numerateur;
+	
+	@Override
+	public String toString() {
+		return "Fraction [denominateur=" + denominateur + ", numerateur=" + numerateur + "]";
+	}
+
+	public Fraction(int denominateur, int numerateur) {
+		super();
+		this.denominateur = denominateur;
+		this.numerateur = numerateur;
+	}
+	
+	
+	 	
+
+}
