a42f5883d3b1fc17824b5c1ece1375a2df16f246
MouhamadouMoustapha <Mouhamadou Moustapha Dia>
2019-09-18 15:38:35 +0200

    ignorer les fichiers .class

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,8 @@ public class Main {
 		System.out.println("Hello World");
 		System.out.println("Bonsoir");
 		System.out.println("ok");
+		System.out.println("Bonsoir");
+		System.out.println("ok");
 		}
 
 }
